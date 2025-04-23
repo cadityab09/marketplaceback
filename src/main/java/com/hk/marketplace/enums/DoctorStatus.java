597328df39523ccbc6721d8a7537f673d4ecd2cc
Aditya Chavan <127530500+cadityab09@users.noreply.github.com>
2025-04-24 00:05:07 +0530

    cms delete

--- src/main/java/com/hk/marketplace/enums/DoctorStatus.java
+++
@@ -1,6 +0,0 @@
-package com.hk.marketplace.enums;
-
-public enum DoctorStatus {
-    ENABLED,
-    DISABLED
-}
--- src/main/java/com/hk/marketplace/enums/EnquiryStatus.java
+++
@@ -1,6 +0,0 @@
-package com.hk.marketplace.enums;
-
-public enum EnquiryStatus {
-    READ,
-    UNREAD
-}
